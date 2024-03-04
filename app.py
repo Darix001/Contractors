2103162f2167cbb297f79f0c18c07cac5e4f6824
Darix001 <100180688+Darix001@users.noreply.github.com>
2024-03-03 22:27:46 -0400

    Login connection and Two forms more added.

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-import PySimpleSQL.db as sql, PySimpleSQL.model as model
-from flask import Flask
-
-db = sql.connect('webapp_db.db')
+from flask import Flask,render_template,request
+from db import database,Usuarios
 
 app = Flask(__name__)
 
-@app.route('/')
+query = Usuarios.select()
+
+@app.before_request
+def _db_connect():
+    database.connect()
+
+
+@app.teardown_request
+def _db_close(exc):
+    if not database.is_closed():
+        database.close()
+
+
+@app.route('/',methods=('POST','GET'))
 def main():
-    return '<h1>Hello World!</h1>'
+    if request.method == 'POST':
+        form = request.form
+        exists = query.where(Usuarios.usuario==form['Usuario'],
+            Usuarios.clave==form['Clave']).exists()
+        if exists:
+            return render_template('Home.html')
+    return render_template('Login.html')
+
+
+@app.route("/Register")
+def register():
+    return render_template('Register.html')
+
+
+@app.route("/Password")
+def password():
+    return render_template('Password.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ db.py
@@ -0,0 +1,68 @@
+from peewee import *
+
+database = SqliteDatabase('webapp_db.db')
+
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+
+class Usuarios(BaseModel):
+    id_usuario = AutoField(column_name='IDUsuario', null=True)
+    nombre = CharField(column_name='Nombre', null=True)
+    apellido = CharField(column_name='Apellido', null=True)
+    email = CharField(column_name='Email', null=True)
+    usuario = CharField(column_name='Usuario', null=True)
+    clave = CharField(column_name='Clave', null=True)
+    profesion = CharField(column_name='Profesion', null=True)
+    telefono = IntegerField(column_name='Telefono', null=True)
+
+
+class Modalidad(BaseModel):
+    id_modalidad = AutoField(column_name='IdModalidad', null=True)
+    nombre = CharField(column_name='Nombre', null=True)
+
+
+class Publicaciones(BaseModel):
+    id_publicacion = AutoField(column_name='IDPublicacion', null=True)
+    id_usuario = ForeignKeyField(column_name='IDUsuario', field='id_usuario', model=Usuarios, null=True)
+    titulo = CharField(column_name='Titulo', null=True)
+    descripcion = CharField(column_name='Descripcion', null=True)
+    imagen = CharField(column_name='Imagen', null=True)
+    localizacion = CharField(column_name='Localizacion', null=True)
+    modalidad = ForeignKeyField(column_name='Modalidad', field='id_modalidad', model=Modalidad, null=True)
+    area = CharField(column_name='Area', null=True)
+
+
+class Comentario(BaseModel):
+    id_comentario = AutoField(column_name='IdComentario', null=True)
+    id_publicacion = ForeignKeyField(column_name='IdPublicacion', field='id_publicacion', model=Publicaciones, null=True)
+    id_usuario = ForeignKeyField(column_name='IdUsuario', field='id_usuario', model=Usuarios, null=True)
+    comentario = CharField(column_name='Comentario', null=True)
+
+
+class Estatus(BaseModel):
+    id_estatus = AutoField(column_name='IdEstatus', null=True)
+    nombre = CharField(column_name='Nombre', null=True)
+
+
+class EtiquetaReaccion(BaseModel):
+    id_etiqueta = AutoField(column_name='IdEtiqueta', null=True)
+    nombre = CharField(column_name='Nombre', null=True)
+
+
+class Reacciones(BaseModel):
+    id_reaccion = AutoField(column_name='IdReaccion', null=True)
+    id_publicacion = ForeignKeyField(column_name='IdPublicacion', field='id_publicacion', model=Publicaciones, null=True)
+    id_usuario = ForeignKeyField(column_name='IdUsuario', field='id_usuario', model=Usuarios, null=True)
+    reaccion = ForeignKeyField(column_name='Reaccion', field='id_etiqueta', model=EtiquetaReaccion, null=True)
+
+
+class Solicitudes(BaseModel):
+    id_solicitud = AutoField(column_name='IdSolicitud', null=True)
+    solicitante = ForeignKeyField(column_name='Solicitante', field='id_usuario', model=Usuarios, null=True)
+    id_publicacion = ForeignKeyField(column_name='IdPublicacion', field='id_publicacion', model=Publicaciones, null=True)
+    estatus = ForeignKeyField(column_name='Estatus', field='id_estatus', model=Estatus, null=True)
+    comentario = CharField(column_name='Comentario', null=True)
+    calificacion = IntegerField(column_name='Calificacion', null=True)
