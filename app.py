5d6c92bff4805c664edd0914d2511e00b8bb38a2
Darix001 <100180688+Darix001@users.noreply.github.com>
2024-04-05 16:55:04 -0400

    Cambios finales
    Cambios finales realizados. Está funcional y listo para ser utilizado.

--- app.py
+++ app.py
@@ -2,10 +2,22 @@ from flask import Flask, render_template, request, redirect, url_for
 from validate_email import validate_email
 from db import database, Usuarios
 from itertools import filterfalse
+from functools import partial
+from smtplib import SMTP_SSL
+from types import MethodType
+from random import choice
+from orjson import loads
+from os import urandom
 
 app = Flask(__name__)
 
-methods = ('POST', 'GET')
+app.route = partial(app.route, methods = ('POST','GET'))
+
+choice = MethodType(choice, range(10000000, 100000000))
+
+with open('config.json') as config:
+    config = loads(config.read())
+
 
 @app.before_request
 def connect():
@@ -17,25 +29,27 @@ def close(exc, /):
         database.close()
 
 def route(obj, /):
-    def function(filename=(name:=obj.__name__) + '.html', value=None, text=''):
+    name = obj.__name__
+    filename = name + '.html'
+    def function(value=None, text='',/, **kwargs):
         if (req := request).method == 'POST':
-            value, text = obj(req.form)
+            value, text = obj(req.form,kwargs)
         return value or render_template(filename, text=text)
     function.__name__ = name
-    return app.route('/' + name, methods=methods)(function)
+    return app.route('/' + name)(function)
 
-@app.route('/', methods=methods)
+@app.route('/')
 def main():
     return redirect(url_for('Home'))
 
-@app.route('/login', methods=methods)
+@app.route('/login')
 def login(text='Login'):
     if request.method == 'POST':
         form = request.form
         usuario = form.get('Usuario')
         clave = form.get('Clave')
         if not usuario or not clave:
-            text = 'Por favor ingrese el usuario y la clave.'
+            text = 'Please Introduce Username and Password'
         else:
             try:
                 usuario_db = Usuarios.get(
@@ -49,29 +63,68 @@ def login(text='Login'):
     return render_template('Login.html', text=text)
 
 @route
-def Register(form, data={}, /):
-    data |= form.items()
-    if keys := ','.join(filterfalse(data.get, data)):
+def Register(form, data, /):
+    app.current_user = usuario = Usuarios(**form)
+    if keys := ','.join(filterfalse(None, form.values())):
         return None, f'Missing fields: {keys}'
-    elif not validate_email(data['email']):
+    elif not validate_email(usuario.email):
         return None, 'The Email address does not exist.'
-    elif not Usuarios(**data).save():
+    elif not usuario.save():
         return None, 'Register Error, report to the administrator.'
     else:
-        return redirect(url_for('login')), None
+        return redirect(url_for('email', name = 'login')),None
+
+@app.route('/email/<name>')
+def email(name:str):
+    app.code = code = f'{choice()}'
+    email = app.current_user.email
+    msg = config['msg'].format_map(locals())
+    with SMTP_SSL('smtp.gmail.com') as srv:
+        srv.login(config['gmail'], config['key'])
+        srv.sendmail(config['gmail'], email, msg)
+    return redirect(url_for('Code', name = name))
 
 @route
-def Password(form, /):
+def Code(form, data, /):
+    if form['code'] == app.code:
+        return redirect(url_for(request.args.get('name'))),None
+    return 'Invalid Verification Code',None
+
+@route
+def Forgot_Password(form, data, /):
+    try:
+        app.current_user = Usuarios.get(
+            Usuarios.usuario == form['usuario'],
+            Usuarios.email == form['email']
+        )
+    except Usuarios.DoesNotExist:
+        return 'Invalid Username or Email',None
+    else:
+        return redirect(url_for('email', name='Password')),None
+
+@route
+def Password(form, data, /):
+    if not all(form.values()):
+        return None,'There are Missing Fields'
+    if form['new_password'] != (key := form['confirm_password']):
+        return None,'Passwords does not match.'
+    usuario = app.current_user
+    usuario.clave = key
+    usuario.save()
+    return redirect(url_for('HomeUser')),None
+
+@route
+def Home(form,/):
     pass
 
 @route
-def Home(form, /):
-    pass
-
-@route
-def HomeUser(form, /):
+def HomeUser(form,/):
     pass
 
 if __name__ == '__main__':
-    app.secret_key = b'tvqfs!tfdsfu!lfz'
-    app.run(debug=True)
+    app.config.update(
+        SECRET_KEY = urandom(32),
+        DEBUG = True,
+        TESTING = True,
+        )
+    app.run()
--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 from peewee import *
+from datetime import datetime
 
 database = SqliteDatabase('webapp_db.db')
 
@@ -9,63 +10,109 @@ class BaseModel(Model):
 
 class Usuarios(BaseModel):
     id_usuario = AutoField(column_name='IDUsuario', null=True)
+    
     nombre = CharField(column_name='Nombre', null=True)
+    
     apellido = CharField(column_name='Apellido', null=True)
+    
     email = CharField(column_name='Email', null=True)
+    
     usuario = CharField(column_name='Usuario', null=True)
+    
     clave = CharField(column_name='Clave', null=True)
+    
     profesion = CharField(column_name='Profesion', null=True)
+    
     telefono = IntegerField(column_name='Telefono', null=True)
 
 
 class Modalidad(BaseModel):
     id_modalidad = AutoField(column_name='IdModalidad', null=True)
+    
     nombre = CharField(column_name='Nombre', null=True)
 
 
 class Publicaciones(BaseModel):
     id_publicacion = AutoField(column_name='IDPublicacion', null=True)
-    id_usuario = ForeignKeyField(column_name='IDUsuario', field='id_usuario', model=Usuarios, null=True)
+    
+    id_usuario = ForeignKeyField(column_name='IDUsuario', field='id_usuario',
+        model=Usuarios, null=True)
+    
     titulo = CharField(column_name='Titulo', null=True)
+    
     descripcion = CharField(column_name='Descripcion', null=True)
+    
     imagen = CharField(column_name='Imagen', null=True)
+    
     localizacion = CharField(column_name='Localizacion', null=True)
-    modalidad = ForeignKeyField(column_name='Modalidad', field='id_modalidad', model=Modalidad, null=True)
+    
+    modalidad = ForeignKeyField(column_name='Modalidad', field='id_modalidad',
+        model=Modalidad, null=True)
+    
     area = CharField(column_name='Area', null=True)
+    
+    fecha = DateTimeField(column_name='Fecha', default=datetime.now)
+
+    class Meta:
+        database = database
+        indexes = ((('IdUsuario', 'Fecha'), False),)
 
 
 class Comentario(BaseModel):
     id_comentario = AutoField(column_name='IdComentario', null=True)
-    id_publicacion = ForeignKeyField(column_name='IdPublicacion', field='id_publicacion', model=Publicaciones, null=True)
-    id_usuario = ForeignKeyField(column_name='IdUsuario', field='id_usuario', model=Usuarios, null=True)
+    
+    id_publicacion = ForeignKeyField(column_name='IdPublicacion',
+        field='id_publicacion', model=Publicaciones, null=True)
+    
+    id_usuario = ForeignKeyField(column_name='IdUsuario',
+        field='id_usuario', model=Usuarios, null=True)
+    
     comentario = CharField(column_name='Comentario', null=True)
 
 
 class Estatus(BaseModel):
     id_estatus = AutoField(column_name='IdEstatus', null=True)
+    
     nombre = CharField(column_name='Nombre', null=True)
 
 
 class EtiquetaReaccion(BaseModel):
     id_etiqueta = AutoField(column_name='IdEtiqueta', null=True)
+    
     nombre = CharField(column_name='Nombre', null=True)
 
 
 class Reacciones(BaseModel):
     id_reaccion = AutoField(column_name='IdReaccion', null=True)
-    id_publicacion = ForeignKeyField(column_name='IdPublicacion', field='id_publicacion', model=Publicaciones, null=True)
-    id_usuario = ForeignKeyField(column_name='IdUsuario', field='id_usuario', model=Usuarios, null=True)
-    reaccion = ForeignKeyField(column_name='Reaccion', field='id_etiqueta', model=EtiquetaReaccion, null=True)
+
+    id_publicacion = ForeignKeyField(column_name='IdPublicacion',
+        field='id_publicacion', model=Publicaciones, null=True)
+
+    id_usuario = ForeignKeyField(column_name='IdUsuario',
+        field='id_usuario', model=Usuarios, null=True)
+    
+    reaccion = ForeignKeyField(column_name='Reaccion',
+        field='id_etiqueta', model=EtiquetaReaccion, null=True)
 
 
 class Solicitudes(BaseModel):
     id_solicitud = AutoField(column_name='IdSolicitud', null=True)
-    solicitante = ForeignKeyField(column_name='Solicitante', field='id_usuario', model=Usuarios, null=True)
-    id_publicacion = ForeignKeyField(column_name='IdPublicacion', field='id_publicacion', model=Publicaciones, null=True)
-    estatus = ForeignKeyField(column_name='Estatus', field='id_estatus', model=Estatus, null=True)
+ 
+    solicitante = ForeignKeyField(column_name='Solicitante',
+        field='id_usuario', model=Usuarios, null=True)
+ 
+    id_publicacion = ForeignKeyField(column_name='IdPublicacion',
+        field='id_publicacion', model=Publicaciones, null=True)
+    
+    estatus = ForeignKeyField(column_name='Estatus',
+        field='id_estatus', model=Estatus, null=True)
+  
     comentario = CharField(column_name='Comentario', null=True)
+  
     calificacion = IntegerField(column_name='Calificacion', null=True)
 
 
 database.create_tables((Usuarios,Modalidad,Publicaciones,Comentario,Estatus,
     EtiquetaReaccion,Reacciones,Solicitudes))
+
+del datetime
