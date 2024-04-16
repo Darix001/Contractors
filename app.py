f1a1d4b9321c5b8721c3edcad618225088d06d89
Darix001 <100180688+Darix001@users.noreply.github.com>
2024-04-16 00:22:35 -0400

    Edit Form Completed

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for
-from db import database, Usuarios, Publicaciones
+from db import database, Usuarios
 from validate_email import validate_email
 from functools import partial, update_wrapper
-from itertools import filterfalse
 from peewee import IntegrityError
+from itertools import groupby
 from smtplib import SMTP_SSL
-from types import MethodType
 from secrets import choice
 from orjson import loads
 from os import urandom
 
 app = Flask(__name__)
 
-app.route = partial(app.route, methods = ('POST','GET'))
+app.route = partial(app.route, methods=('POST', 'GET'))
 
-choice = MethodType(choice, range(10000000, 100000000))
+keys = range(10000000, 100000000)
 
 with open('config.json', 'rb') as config:
     config = loads(config.read())
-
 
 @app.before_request
 def connect():
@@ -40,8 +38,7 @@ def post(obj=None, /, text=''):
                     return text
             return render_template(filename, text=text)
         return app.route('/' + name)(update_wrapper(function, obj))
-    else:
-        return partial(post, text=text)
+    return partial(post, text=text)
 
 @app.route('/')
 def main():
@@ -57,23 +54,24 @@ def Login(form, /):
     except Usuarios.DoesNotExist:
         return 'Incorrect user or password'
     else:
+        print(app.current_user.__data__)
         return redirect(url_for('HomeUser'))
 
 @post(text = 'Register a new account')
 def Register(form, /):
     app.current_user = usuario = Usuarios(**form)
     if not validate_email(usuario.email):
-        return 'The Email address does not exist.'
+        return 'The Email address does not exist'
     try:
         usuario.save()
     except IntegrityError:
-        return 'Invalid Username'
+        return 'This Username already exists'
     else:
         return redirect(url_for('email', name = 'HomeUser'))
 
 @app.route('/email/<name>')
 def email(name:str):
-    app.code = code = f'{choice()}'
+    app.code = code = f'{choice(keys)}'
     email = app.current_user.email
     msg = config['msg'].format_map(locals())
     with SMTP_SSL('smtp.gmail.com') as srv:
@@ -92,8 +90,7 @@ def Forgot_Password(form, /):
     try:
         app.current_user = Usuarios.get(
             Usuarios.usuario == form['usuario'],
-            Usuarios.email == form['email']
-        )
+            Usuarios.email == form['email'])
     except Usuarios.DoesNotExist:
         return 'Invalid Username or Email'
     else:
@@ -124,7 +121,7 @@ def user_page(obj):
     filename = name + '.html'
     def function():
         if not (usuario := getattr(app, 'current_user', None)):
-            redirect(url_for('Login'))
+            return redirect(url_for('Login'))
         if request.method == 'POST':
             return obj(request.form)
         return render_template(filename, **usuario.__data__)
@@ -133,24 +130,25 @@ def user_page(obj):
 @user_page
 def Edit(form, /):
     usuario = app.current_user
-    usuario.__data__.update(form)
+    data = usuario.__data__
+    json_names = usuario.json_names
+    data|={k:(v if k in json_names else v[0]) for k,v in dict.items(form)}
+    for field, keys in usuario.json_columns.items():
+        data[field] = [*zip(*map(data.pop, keys))]
     usuario.save()
     return redirect(url_for('HomeUser'))
 
+@user_page
+def HomeUser(form, /):
+    pass
 
-@app.route('/HomeUser')
-def HomeUser():
-    data = app.current_user.__data__
-    return render_template('HomeUser.html', **data)
-
-@app.route('/UserProfile')
-def user_profile():
-    return render_template('UserProfile.html')
+@user_page
+def UserProfile(form, /):
+    pass
 
 if __name__ == '__main__':
     app.config.update(
         SECRET_KEY = urandom(32),
         DEBUG = True,
-        TESTING = True,
-        )
+        TESTING = True)
     app.run()
